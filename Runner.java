3ec559d34788efe180afb18cc31ad0533ce2e0e3
MikhailBouzdalkin <32449298+MikhailBouzdalkin@users.noreply.github.com>
2017-10-30 09:20:59 +0300

    Add files via upload

--- Runner.java
+++ Runner.java
@@ -17,49 +17,15 @@ public class Runner {
         }
         System.out.println(empl);
         File f = new File("Text.txt");
-        ObjectOutputStream fo = null;
-
-
-        try {
-            fo = new ObjectOutputStream(new FileOutputStream(f));
+        try(ObjectOutputStream fo = new ObjectOutputStream(new FileOutputStream(f))) {
             fo.writeObject(empl);
-
-        } catch (FileNotFoundException e) {
-        } catch (IOException e) {
-
-        } finally {
-            try {
-                if (fo != null) {
-                    fo.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        }  catch (IOException e) {
+            e.printStackTrace();
         }
-        ObjectInputStream fi = null;
-        try {
-            fi = new ObjectInputStream(new FileInputStream(f));
+        try(ObjectInputStream fi = new ObjectInputStream(new FileInputStream(f))) {
             newEmpl = (Employee) fi.readObject();
-
-        } catch (ClassNotFoundException e) {
-            System.err.println("Cannot read the class");
+        } catch (ClassNotFoundException | IOException e) {
             e.printStackTrace();
-        } catch (FileNotFoundException e) {
-            System.err.println("Cannot find the file");
-            e.printStackTrace();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (fi != null) {
-                    fi.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-
         }
         System.out.println(newEmpl);
     }
